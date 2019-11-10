6893a711fec9744841a2c35e84f2ed4be8301528
cor <cor90@qq.com>
2019-11-10 12:28:03 +0800

    提取公共的 BaseServlet

---
+++ src/main/java/com/example/servlet/BaseServlet.java
@@ -0,0 +1,31 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * 提取公共的 BaseServlet，这样就可以实现每一个类对应一个 Servlet。
+ */
+public class BaseServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // 假设路径为：/travel/user/add
+        String requestURI = req.getRequestURI();
+        // 获取到的方法名为：/add
+        String methodName = requestURI.substring(requestURI.lastIndexOf("/") + 1);
+        // 使用反射调用对应的方法
+        try {
+            Method method = this.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(this, req, resp);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
